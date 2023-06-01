f57c20ebe35c2f7df542918ccca13cb60ee4c69f
ArjunKhade <106642798+ArjunKhade@users.noreply.github.com>
2023-06-02 00:14:53 +0530

    education done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BanerComponent } from './baner/baner.component';
 import { ResponsiveDirective } from './responsive.directive';
+import { PersonalInformationComponent } from './personal-information/personal-information.component';
+import { EducationComponent } from './education/education.component';
+import { WorkExperienceComponent } from './work-experience/work-experience.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     BanerComponent,
-    ResponsiveDirective
+    ResponsiveDirective,
+    PersonalInformationComponent,
+    EducationComponent,
+    WorkExperienceComponent
   ],
   imports: [
     BrowserModule,
--- src/app/responsive.directive.ts
+++ src/app/responsive.directive.ts
@@ -12,14 +12,13 @@ export class ResponsiveDirective {
         next: (result: any) => {
           console.log(result);
           for (let breakpoint of Object.keys(result.breakpoints)) {
-            if (result.breakpoints[breakpoint])
+            if (result.breakpoints[breakpoint]) {
               if (breakpoint === Breakpoints.HandsetPortrait) {
-                console.log('mob');
                 this.element.nativeElement.classList.remove('pc');
               }
-            if (breakpoint === Breakpoints.WebLandscape) {
-              console.log('web');
-              this.element.nativeElement.classList.add('pc');
+              if (breakpoint === Breakpoints.WebLandscape) {
+                this.element.nativeElement.classList.add('pc');
+              }
             }
           }
         },
